508a8ca7d56c30960aeca6847edd69ec13e37d91
xunlin <scott_lx@qq.com>
2022-08-26 17:15:24 +0800

    dpdk ringbuffer go implementation

---
+++ src/ringbuffer/ringbuffer.go
@@ -0,0 +1,115 @@
+package ringbuffer
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+type Ring struct {
+	consHead uint32
+	consTail uint32
+	prodHead uint32
+	prodTail uint32
+	mem      []string
+	length   uint32
+}
+
+func NewRing(size uint32) *Ring {
+	return &Ring{
+		consHead: 0,
+		consTail: 0,
+		prodHead: 0,
+		prodTail: 0,
+		mem:      make([]string, size),
+		length:   size,
+	}
+}
+
+func (r *Ring) Enqueue(elem string) {
+	err := r.enqueue(elem)
+	for err != nil {
+		err = r.enqueue(elem)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
+
+func (r *Ring) enqueue(elem string) error {
+	var localProdNext, localProdHead, localConsTail uint32
+	localProdHead = atomic.LoadUint32(&r.prodHead)
+	localConsTail = atomic.LoadUint32(&r.consTail)
+	if localProdHead < r.length-1 {
+		localProdNext = localProdHead + 1
+	}
+
+	if localConsTail > localProdHead {
+		return errors.New("buffer is full")
+	}
+
+	if success := atomic.CompareAndSwapUint32(&r.prodHead, localProdHead, localProdNext); success {
+		r.mem[localProdHead] = elem
+		fmt.Println(elem, " writted")
+		for r.prodTail != localProdHead {
+			//fmt.Println("waiting others finish writing")
+		}
+		r.prodTail = localProdNext
+		fmt.Println(r)
+		return nil
+	} else {
+		fmt.Println("preempted by others")
+		return errors.New("preempted by others")
+	}
+}
+
+func (r *Ring) Dequeue() (string, error) {
+	var localConsHead, localConsNext, localProdTail uint32
+	var res string
+
+	localConsHead = atomic.LoadUint32(&r.consHead)
+	localProdTail = atomic.LoadUint32(&r.prodTail)
+	if localConsHead != r.length-1 {
+		localConsNext = localConsHead + 1
+	}
+
+	if localConsHead >= localProdTail {
+		return "", errors.New("buffer is empty")
+	}
+
+	r.consHead = localConsNext
+	res = r.mem[localConsHead]
+	r.consTail = r.consHead
+
+	return res, nil
+}
+
+func Test() {
+	r := NewRing(5)
+	var wg sync.WaitGroup
+	wg.Add(10)
+
+	var locker = new(sync.Mutex)
+	var cond = sync.NewCond(locker)
+
+	for i := 0; i < 5; i++ {
+		go func(elem int) {
+			defer wg.Done()
+
+			cond.L.Lock()
+			cond.Wait()
+
+			r.Enqueue(strconv.Itoa(elem))
+
+			cond.L.Unlock()
+
+		}(i)
+	}
+
+	time.Sleep(time.Second * 1)
+	cond.Broadcast()
+	wg.Wait()
+}
--- src/test.go
+++ src/test.go
@@ -2,41 +2,28 @@ package main
 
 import (
 	"fmt"
-	"math/big"
-	"net"
+	"go-base/src/ringbuffer"
+	"strconv"
 )
 
-type addr struct {
-	ip4 string
-	ip6 string
-}
-
 func main() {
-	i := ip6ToInt("::d:1:1:0:101")
-	fmt.Println(i)
+	r := ringbuffer.NewRing(5)
+	for i := 0; i < 4; i++ {
+		r.Enqueue(strconv.Itoa(i))
+	}
+	for i := 0; i < 5; i++ {
+		if str, err := r.Dequeue(); err == nil {
+			fmt.Println("get ", str)
+		} else {
+			fmt.Println("error: ", err)
+		}
+	}
+	r.Enqueue(strconv.Itoa(5))
+	if str, err := r.Dequeue(); err == nil {
+		fmt.Println("get ", str)
+	} else {
+		fmt.Println("error: ", err)
+	}
 
-	i.Add(i, big.NewInt(11))
-	fmt.Println(i)
-	fmt.Println(intToNet6(i))
-}
-
-func ip6ToInt(ip string) *big.Int {
-	ret := big.NewInt(0)
-	fmt.Println([]byte(net.ParseIP(ip).To16()))
-	ret.SetBytes([]byte(net.ParseIP(ip).To16()))
-	fmt.Println(ret)
-	return ret
-}
-
-func intToNet6(ip *big.Int) string {
-	b := make([]byte, 16-len(ip.Bytes()))
-	b = append(b, ip.Bytes()...)
-	return net.IP(b).String() + "/32"
-}
-
-func net6ToInt(inet string) *big.Int {
-	ip, _, _ := net.ParseCIDR(inet)
-	ret := big.NewInt(0)
-	ret.SetBytes(ip.To16())
-	return ret
+	//ringbuffer.Test()
 }
